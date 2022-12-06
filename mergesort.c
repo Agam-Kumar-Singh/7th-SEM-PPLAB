f7494094870c6a386e45e13ecf1362f22fa262f0
Agam Kumar Singh <80572287+Agam-Kumar-Singh@users.noreply.github.com>
2022-12-07 00:16:40 +0530

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -1,72 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<omp.h>
 
-#include <omp.h>
+void merge(int *a,int i1,int j1,int i2,int j2){
 
-void mergesort(int a[],int i,int j);
-void merge(int a[],int i1,int j1,int i2,int j2);
+    int i=i1;
+    int j=i2;
+    int k=0;
 
-int main()
-{
-    int *a, num, i;
-    printf("Enter Number of elements:\n");
-    scanf("%d",&num);
-
-   a = (int *)malloc(sizeof(int) * num);
-   printf("Enter all elements to be sorted\n");
-    for(i=0;i<num;i++)
-        scanf("%d",&a[i]);
-
-    double start_time=omp_get_wtime();   
-    mergesort(a, 0, num-1);
-    double end_time=omp_get_wtime();
-
-    double value=end_time-start_time;
-    printf("Time taken for execution is: %f\n",value);
-    
-    printf("\nSorted array :\n");
-    for(i=0;i<num;i++)
-        printf("%d ",a[i]);
-        
-    return 0;
-}
- 
-void mergesort(int a[],int i,int j)
-{
-    int mid;
-        
-    if(i<j)
-    {
-        mid=(i+j)/2;
-        
-        #pragma omp parallel sections 
-        {
-
-            #pragma omp section
-            {
-                printf("Thread: %d\n",omp_get_num_threads());
-                mergesort(a,i,mid);        //left recursion
-            }
-
-            #pragma omp section
-            {
-                printf("Thread: %d\n",omp_get_num_threads());
-                mergesort(a,mid+1,j);    //right recursion
-            }
-        }
-
-        merge(a,i,mid,mid+1,j);    //merging of two sorted sub-arrays
-    }
-}
- 
-void merge(int a[],int i1,int j1,int i2,int j2)
-{
-    int temp[1000];    //array used for merging
-    int i,j,k;
-    i=i1;    //beginning of the first list
-    j=i2;    //beginning of the second list
-    k=0;
-    
+    int temp[1000];
     while(i<=j1 && j<=j2)    //while elements in both lists
     {
         if(a[i]<a[j])
@@ -74,14 +16,69 @@ void merge(int a[],int i1,int j1,int i2,int j2)
         else
             temp[k++]=a[j++];
     }
-    
+
     while(i<=j1)    //copy remaining elements of the first list
         temp[k++]=a[i++];
         
     while(j<=j2)    //copy remaining elements of the second list
         temp[k++]=a[j++];
-        
-    //Transfer elements from temp[] back to a[]
+    
     for(i=i1,j=0;i<=j2;i++,j++)
-        a[i]=temp[j];
+    a[i]=temp[j];
+
+}
+
+void mergesortParallel(int *a,int l,int h){
+    if(l<h){
+
+        int mid=(l+h)/2;
+
+        #pragma omp parallel sections
+        {
+            #pragma omp section
+            {  
+                mergesortParallel(a,l,mid);
+            }
+            #pragma omp section
+            {  
+                mergesortParallel(a,mid+1,h);
+            }
+        }
+        merge(a,l,mid,mid+1,h);
+    }
+}
+
+void mergesortSerial(int *a,int l,int h){
+    if(l<h){
+
+        int mid=(l+h)/2;
+                mergesortSerial(a,l,mid);
+                mergesortSerial(a,mid+1,h);
+        merge(a,l,mid,mid+1,h);
+    }
+}
+int main(){
+
+    int a[100],n;
+    printf("Enter number of elements: ");
+    scanf("%d",&n);
+    printf("Enter elements: ");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+
+    double start=omp_get_wtime();
+    mergesortParallel(a,0,n-1);
+    double end=omp_get_wtime();
+    printf("\nTime for parallel execution is: %lf",end-start);
+
+    start=omp_get_wtime();
+    mergesortSerial(a,0,n-1);
+    end=omp_get_wtime();
+    printf("\nTime for Serial execution is: %lf",end-start);
+
+    printf("Array after sorting:\n");
+    for(int i=0;i<n;i++)
+    printf("%d ",a[i]);
+
 }
