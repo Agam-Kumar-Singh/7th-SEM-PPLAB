aca8eb03ea4dc4321d5e07c5b4ae7ac801c3c7c5
Agam Kumar Singh <80572287+Agam-Kumar-Singh@users.noreply.github.com>
2022-12-06 23:39:09 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,42 +1,47 @@
-//Write a program to find the prime numbers from 1 to n employing parallel for directive. Record both serial and parallel execution times.
-
 #include<stdio.h>
+#include<stdlib.h>
 #include<omp.h>
+
+
+int isPrime(int x){
+    if(x<=2)
+    return 0;
+    else
+    for(int i=2;i<=x/2;i++)
+    if(x%i==0)
+    return 0;
+    return 1;
+
+}
+void parallelprime(int n){
+    
+    printf("Prime Numbers are; \n");
+    #pragma omp parallel for
+    for(int i=2;i<=n;i++){
+        if(isPrime(i))
+        printf("%d ",i);
+    }
+}
+
+void serialprime(int n){
+    
+    printf("Prime Numbers are; \n");
+    for(int i=2;i<=n;i++){
+        if(isPrime(i))
+        printf("%d ",i);
+    }
+}
+
 
 int main(){
 
-    int prime[1000];
-    printf("\nIn order to find prime numbers from 1 to n, enter the value of n:");
-    int n;
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        prime[i]=1;
-    }
+   double start=omp_get_wtime();
+   parallelprime(100);
+   double end=omp_get_wtime();
+   printf("Time for Parallel execution is %lf\n",end-start);
 
-    prime[1]=0;
-    
-    double start_time=omp_get_wtime();
-    for(int i=2;i*i<=n;i++){
-
-        #pragma omp parallel for
-        for(int j=i*i;j<=n;j+=i)
-        {
-            if(prime[j]==1)
-            prime[j]=0;
-        }
-    }
-    double end_time=omp_get_wtime();
-    printf("Execution time for parallel region: %lf\n",end_time-start_time);
-
-    printf("\nPrime numbers from 1 to %d are\n",n);
-
-    for(int i=2;i<=n;i++)
-    {
-        if(prime[i] == 1)
-        {
-            printf("%d\t ",i);
-        }
-    }
-
-    return 0;
+   start=omp_get_wtime();
+   serialprime(100);
+    end=omp_get_wtime();
+   printf("Time for Serial execution is %lf\n",end-start);
 }
