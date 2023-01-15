f54a6bac2bb0b1fcf56b9be392b782b0891a2c33
Agam Kumar Singh <80572287+Agam-Kumar-Singh@users.noreply.github.com>
2023-01-15 09:34:19 +0530

    Update sumNumbers.c

--- sumNumbers.c
+++ sumNumbers.c
@@ -1,18 +1,38 @@
 //Write a program   calculate the sum of first 100 Numbers. (Using critical directive)
 
 #include<stdio.h>
-#include<stdlib.h>
 #include<omp.h>
 
 int main(){
 
-    int sum=0;
-    #pragma omp critical
-    for(int i=1;i<=100;i++){
-        sum+=i;
-        printf("Thread in use:%d \n",omp_get_num_threads());
+    int n=100;
+    double sum=0;
+
+    // #pragma omp parallel for shared(sum)
+    // for(int i=1;i<=n;i++){
+    // #pragma omp critical
+    // sum+=i;
+    // }
+
+    // printf("Sum is: %d ",sum);
+
+    double thread[100];
+    #pragma omp parallel for num_threads(8)
+    for(int i=1;i<=100;i++)
+    {
+        #pragma omp critical
+        {   
+            int t=omp_get_thread_num();
+            thread[t]+=i;
+            printf("Thread %d has now value stored: %lf\n",t,thread[t]);
+        }
+
     }
-    printf("Sum=%d ",sum);
+
+    for(int i=0;i<8;i++)
+    sum+=thread[i];
+
+    printf("Sum is %lf ",sum);
 }
 
 //gcc -fopenmp sumNumbers.c -o sum
