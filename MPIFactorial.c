91bdcdbe7d5e478642a9e674d3c51c4525e8349b
Agam Kumar Singh <80572287+Agam-Kumar-Singh@users.noreply.github.com>
2023-01-15 17:14:53 +0530

    Update MPIFactorial.c

--- MPIFactorial.c
+++ MPIFactorial.c
@@ -1,38 +1,67 @@
 //Write a MPI program to find factorial of a number using Broadcast and Reduce operation.
 
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<mpi.h>
-
-int main(int argc,char *argv[]){
-
-  int size,rank;
-  int n;
-  printf("Enter number whose factorial is to be found :");
-  scanf("%d",&n);
-  MPI_Init(&argc,&argv);
-  MPI_Comm_size(MPI_COMM_WORLD,&size);
-  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-
-  int root=0;
-  int global;
-
-  MPI_Bcast(&n,1,MPI_INT,root,MPI_COMM_WORLD);
-  int localProd=1;
-  int len=(n/size)+1;
-  for(int i=1;i<=len;i++){
-    int val=(rank*len)+i;
-    if(val<=n)
-      localProd*=val;
-    else break;
+void main(int argc,char * argv[]){
+	int n,rank,size;
+  printf("Enter N: ");
+	scanf("%d",&n);
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	int root=0;
+	
+	int global;	
+	
+	MPI_Bcast(&n,1,MPI_INT,root,MPI_COMM_WORLD);
+	int localprod=1;
+	int len = (n/size);
+  for(int i=(rank*len)+1;i<=(rank+1)*len;i++){
+    if(i>n)
+      break;
+    localprod*=i;
   }
-  printf("Local Product: %d",localProd);
-  MPI_Reduce(&localProd,&global,1,MPI_INT,MPI_PROD,root,MPI_COMM_WORLD);
-
-  if(rank==root)
-  printf("Factorial of %d is %d\n",n,global);
-  MPI_Finalize();
+	printf("LOCAL PROF:%d\n",localprod);
+MPI_Reduce(&localprod,&global,1,MPI_INT,MPI_PROD,root,MPI_COMM_WORLD);
+if(rank==root)
+		printf("Factorial of %d is %d\n",n,global);
+	MPI_Finalize();
 }
+// #include<stdio.h>
+// #include<stdlib.h>
+// #include<mpi.h>
+
+// int main(int argc,char *argv[]){
+
+//   int size,rank;
+//   int n;
+//   printf("Enter number whose factorial is to be found :");
+//   scanf("%d",&n);
+//   MPI_Init(&argc,&argv);
+//   MPI_Comm_size(MPI_COMM_WORLD,&size);
+//   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+//   int root=0;
+//   int global;
+
+//   MPI_Bcast(&n,1,MPI_INT,root,MPI_COMM_WORLD);
+//   int localProd=1;
+//   int len=(n/size)+1;
+//   for(int i=1;i<=len;i++){
+//     int val=(rank*len)+i;
+//     if(val<=n)
+//       localProd*=val;
+//     else break;
+//   }
+//   printf("Local Product: %d",localProd);
+//   MPI_Reduce(&localProd,&global,1,MPI_INT,MPI_PROD,root,MPI_COMM_WORLD);
+
+//   if(rank==root)
+//   printf("Factorial of %d is %d\n",n,global);
+//   MPI_Finalize();
+// }
 
 //mpicc main.c
 //./a.out
